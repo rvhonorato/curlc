023f04c343df9b125ef236719b9d4ffa8f5932ba
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-05-03 12:11:07 +0200

    improve `parse_url`

--- src/url.rs
+++ src/url.rs
@@ -7,17 +7,28 @@ pub struct UrlComponents {
 
 /// This function recieves a "normal" url and retrieves the relevant fields for the UrlComponents
 pub fn parse_url(url: &str) -> UrlComponents {
-    let (_protocol, rest) = url.split_once("://").unwrap();
-    let (tmp_hostname, pathname) = rest.split_once('/').unwrap();
-    let (hostname, port) = if tmp_hostname.contains(':') {
-        tmp_hostname.split_once(':').expect("Invalid hostname")
+    // protocol is http or https
+    let (_protocol, addr) = url.split_once("://").unwrap();
+
+    // extract the hostname and the path from the addr
+    let (hostname_part, path_part) = addr.split_once('/').unwrap_or((addr, ""));
+
+    // extract the port, if any
+    let (hostname, port) = hostname_part
+        .split_once(':')
+        .unwrap_or((hostname_part, "80"));
+
+    // process the path, if any
+    let pathname = if path_part.is_empty() {
+        String::new()
     } else {
-        (tmp_hostname, "80")
+        format!("/{}", path_part)
     };
+
     UrlComponents {
         protocol: "HTTP/1.1".to_string(),
         hostname: hostname.to_string(),
-        pathname: pathname.to_string(),
+        pathname,
         socket_addr: format!("{}:{}", hostname, port),
     }
 }
